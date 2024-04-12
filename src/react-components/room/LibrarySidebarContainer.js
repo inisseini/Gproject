348d4ff7347e4b49406b7adc4bc551a6f3e054b8
inisseini <issei.kurata819@gmail.com>
2024-04-12 09:15:14 +0900

    librarySidebar blink

--- src/react-components/room/LibrarySidebarContainer.js
+++ src/react-components/room/LibrarySidebarContainer.js
@@ -36,13 +36,14 @@ export function LibrarySidebarContainer({ onClose, scene, setQuestion }) {
     }
     return (
       <div
-        onClick={() => {
+        onClick={(e) => {
+          e.preventDefault();
           setQuestion(id);
           scene.emit("add_media", img);
-          /*if(!sessionStorage.getItem("objectTutorial")) {
+          if(!sessionStorage.getItem("objectTutorial")) {
             sessionStorage.setItem("objectTutorial", true);
             alert("オブジェクトは右クリックで詳細を確認することができます。リンクを取得したり、理解度チェックを受けてみてください。")
-          }*/
+          }
         }}
         style={{
           display: "flex",
