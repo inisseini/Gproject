1444ced28a51763c14529ba296c6b6b53f1509a3
inisseini <issei.kurata819@gmail.com>
2024-04-19 10:58:51 +0900

    フレンド申請

--- src/react-components/room/PeopleSidebar.js
+++ src/react-components/room/PeopleSidebar.js
@@ -242,7 +242,7 @@ export function PeopleSidebar({
                       "https://metacampusassets.s3.ap-northeast-1.amazonaws.com/" + person.profile.metacampusID + ".jpg"
                     }
                     alt
-                    onError={e => (this.src = { userDemoImg })}
+                    onerror="this.onerror = null; this.src='';"
                   />
                 </div>
                 <div className={styles.detail}>
