846ec15d09b9e9381fb550ddd949571e9dfe21d8
inisseini <issei.kurata819@gmail.com>
2024-04-25 06:54:06 +0900

    フレンド申請

--- src/react-components/room/PeopleSidebarContainer.js
+++ src/react-components/room/PeopleSidebarContainer.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
+import React, { useCallback, useEffect, useState, useMemo } from "react";
 import PropTypes from "prop-types";
 import { PeopleSidebar } from "./PeopleSidebar";
 import { getMicrophonePresences } from "../../utils/microphone-presence";
