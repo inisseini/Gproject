c5452d33cb8ce1698a9b9c0dc1a3f6155ff2a20f
inisseini <issei.kurata819@gmail.com>
2024-06-17 05:17:04 +0900

    admin

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -1,4 +1,5 @@
 import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import copy from "copy-to-clipboard";
